56d3df761b83d28f4215772cb1c46bf1a485c563
veronicajujuy <veronica.jujuy@gmail.com>
2023-09-06 12:03:07 -0300

    agregando middlewares

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,59 +1,28 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const path = require('path');
-const multer = require('multer');
+// configuracion de multer
+const uploadFile = require("../middlewares/multerMiddleware");
+// configuracion de express-validator
+const validations = require("../middlewares/validatorMiddleware");
 
-const { body } = require('express-validator');
-
-const storage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, './public/images/avatars');
-	},
-	filename: (req, file, cb) => {
-		let fileName = `${Date.now()}_img${path.extname(file.originalname)}`;
-		cb(null, fileName);
-	}
-})
-
-const uploadFile = multer({ storage });
-
-const usersController = require('../controllers/userController');
-
-const validations = [
-	body('fullName').notEmpty().withMessage('Tienes que escribir un nombre'),
-	body('email')
-		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
-		.isEmail().withMessage('Debes escribir un formato de correo válido'),
-	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
-	body('country').notEmpty().withMessage('Tienes que elegir un país'),
-	body('avatar').custom((value, { req }) => {
-		let file = req.file;
-		let acceptedExtensions = ['.jpg', '.png', '.gif'];
-		
-		if (!file) {
-			throw new Error('Tienes que subir una imagen');
-		} else {
-			let fileExtension = path.extname(file.originalname);
-			if (!acceptedExtensions.includes(fileExtension)) {
-				throw new Error(`Las extensiones de archivo permitidas son ${acceptedExtensions.join(', ')}`);
-			}
-		}
-
-		return true;
-	})
-]
+const usersController = require("../controllers/userController");
 
 // Formulario de registro
-router.get('/register', usersController.register);
+router.get("/register", usersController.register);
 
 // Procesar el registro
-router.post('/register', uploadFile.single('avatar'), validations, usersController.processRegister);
+router.post(
+  "/register",
+  uploadFile.single("avatar"),
+  validations,
+  usersController.processRegister
+);
 
 // Formulario de login
-router.get('/login', usersController.login);
+router.get("/login", usersController.login);
 
 // Perfil de Usuario
-router.get('/profile/:userId', usersController.profile);
+router.get("/profile/:userId", usersController.profile);
 
 module.exports = router;
